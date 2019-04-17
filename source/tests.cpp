ef3c103159ba301ea5508b8f77464937caa5f8bc
Molotow5 <v_spassov@web.de>
2019-04-17 17:00:50 +0200

    Anfangscommit

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,17 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
+
+
+
+TEST_CASE("describe_Vec2_struct", "[Vec2]"){
+  Vec2 Testobjekt_1;
+  Vec2 Testobjekt_2{7,288.88};
+  REQUIRE(Testobjekt_1.x == 0);
+  REQUIRE(Testobjekt_1.y == 0);
+  REQUIRE(Testobjekt_2.x == 7);
+  REQUIRE(Testobjekt_2.y == Approx(288.88));
+}
 
 int main(int argc, char *argv[])
 {
---
+++ source/vec2.hpp
@@ -0,0 +1,15 @@
+#ifndef VEC2_HPP
+#define VEC2_HPP
+
+// Für Vektor im 2 Dimensionalen Raum
+struct Vec2{
+
+    double x = 0.0f;
+    double y = 0.0f;
+
+    Vec2& operator+=(Vec2 const& v){
+        
+    }
+};
+
+#endif //END_VEC2

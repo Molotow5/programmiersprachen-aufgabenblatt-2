e6fe093df30a0037debea612e48791bbd31a21e6
Molotow5 <v_spassov@web.de>
2019-04-24 15:32:28 +0200

    Fast funktionierende neue Tests. Mehr Übersicht.

--- source/tests.cpp
+++ source/tests.cpp
@@ -7,23 +7,75 @@ TEST_CASE("describe_Vec2_struct", "[Vec2]"){
   Vec2 Testobjekt_1;
   Vec2 Testobjekt_2{7, 288.88};
   Vec2 Testobjekt_3{-5.5, 200};
-  float lambda = 2;
   REQUIRE(Testobjekt_1.x == 0);
   REQUIRE(Testobjekt_1.y == 0);
   REQUIRE(Testobjekt_2.x == 7);
   REQUIRE(Testobjekt_2.y == Approx(288.88));
+}
+
+TEST_CASE("describe_operator+=", "[operator+=]"){
+  Vec2 Testobjekt_1;
+  Vec2 Testobjekt_2{-7, 288.88f};
   Testobjekt_1 += Testobjekt_2;
-  REQUIRE(Testobjekt_1.x == 7);
-  Testobjekt_2 -= Testobjekt_1;
-  REQUIRE(Testobjekt_2.y == 0);
+  REQUIRE(Testobjekt_1.x == -7);
+  REQUIRE(Testobjekt_1.y == 288.88f);
+}
+
+TEST_CASE("describe_operator*=", "[operator*=]"){
+  Vec2 Testobjekt_3{-5.5, 200};
+  float lambda = 2;
   Testobjekt_3 *= lambda;
+  REQUIRE(Testobjekt_3.x == -11);
   REQUIRE(Testobjekt_3.y == 400);
-  Testobjekt_3 /= lambda;
-  REQUIRE(Approx(Testobjekt_3.x) == -5.5);
-  Vec2 Testobjekt_4 = Testobjekt_3 + Testobjekt_1;
-  REQUIRE(Approx(Testobjekt_4.x) == 1.5);
-  Vec2 Testobjekt_5 = Testobjekt_4 - Testobjekt_1;
-  REQUIRE(Approx(Testobjekt_5.x) == -5.5);
+}
+
+TEST_CASE("describe_operator-=", "[operator-=]"){
+  Vec2 Testobjekt_77{33, 55};
+  Vec2 Testobjekt_88{11, -5};
+  Testobjekt_77 -= Testobjekt_88;
+  REQUIRE(Testobjekt_77.x == 22);
+  REQUIRE(Testobjekt_77.y == 60);
+}
+
+TEST_CASE("describe_operator/=", "[operator/="){
+  Vec2 Testobjekt_71{-18, 72};
+  float lambda = 18;
+  Testobjekt_71 /= lambda;
+  REQUIRE(Testobjekt_71.x == -1);
+  REQUIRE(Testobjekt_71.y == 4);
+}
+
+TEST_CASE("describe_operator+", "[operator+]"){
+  Vec2 Testobjekt_4{5555.55f, -30};
+  Vec2 Testobjekt_5{0.45, 30};
+  Vec2 Testobjekt_6 = Testobjekt_4 + Testobjekt_5;
+  REQUIRE(Testobjekt_6.x == 5556);
+  REQUIRE(Testobjekt_6.y == 0);
+}
+TEST_CASE("describe_operator-", "[operator-]"){
+  Vec2 Testobjekt_7{33, 55};
+  Vec2 Testobjekt_8{11, 5};
+  Vec2 Testobjekt_9 = Testobjekt_7 - Testobjekt_8;
+  REQUIRE(Testobjekt_9.x == 22);
+  REQUIRE(Testobjekt_9.y == 60);
+}
+
+TEST_CASE("describe_operator*", "[operator*]"){
+  Vec2 Testobjekt_10{-5.5, 200};
+  float lambdo = 2.5f;
+  float lambda = 2;
+  Vec2 Testobjekt_11 = Testobjekt_10 * lambdo;
+  REQUIRE(Testobjekt_11.x == -13.75f);
+  REQUIRE(Testobjekt_11.y == 500);
+  Vec2 Testobjekt_12 = lambda * Testobjekt_11;
+  REQUIRE(Testobjekt_12.x == -27.5f);
+  REQUIRE(Testobjekt_12.y == 1000);
+}
+TEST_CASE("describe_operator/", "[operator/]"){
+  Vec2 Testobjekt_13{12000.8f, -44};
+  float zahl = 2;
+  REQUIRE(Testobjekt_13.x == 6000.4f);
+  REQUIRE(Testobjekt_13.y == -22);
 }
 
 int main(int argc, char *argv[])
